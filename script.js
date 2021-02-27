03b55f53cd95c28caf1b7882ef962f079e703f6a
SWFTR <mirko.scheschonka@gmail.com>
2021-02-28 00:26:20 +0100

    finished all JS (I think...)

--- script.js
+++ script.js
@@ -18,16 +18,23 @@ function addItem() {
 
 
     if (document.getElementById('input-field').value === '') {
+
+        // only add an error message if there is not already one
        if (document.getElementsByClassName('error-message-div').length === 0) {
         toDoBoxParent.insertBefore(errorMessageDiv, input)
         }
 }
     else {
+
+//creating and configuring elements...
+
     const createItemBox = document.createElement('div');
 createItemBox.className = 'item-box';
 
 const itemsParagraph = document.createElement('p');
 itemsParagraph.innerHTML = document.getElementById('input-field').value;
+itemsParagraph.contentEditable = 'true';
+itemsParagraph.id = 'item-name';
 //itemsParagraph.innerHTML = 'hello'
 
 const iconsDiv = document.createElement('div');
@@ -38,13 +45,15 @@ checkIcon.className = 'material-icons';
 checkIcon.style.color = 'lime';
 checkIcon.innerHTML = 'check_circle_outline';
 checkIcon.id = 'task-finished'
-checkIcon.addEventListener('click',taskFinished);
+checkIcon.addEventListener('click',taskFinished); 
 
 
 const editIcon = document.createElement('i');
 editIcon.className = 'material-icons';
 editIcon.style.color = '#428bca';
-editIcon.innerHTML = 'create'
+editIcon.innerHTML = 'create';
+editIcon.id = 'edit-icon';
+editIcon.addEventListener('click', editItem)
 
 const deleteIcon = document.createElement('i');
 deleteIcon.className = 'material-icons';
@@ -62,6 +71,8 @@ deleteIcon.addEventListener('click', deleteTask);
     iconsDiv.appendChild(deleteIcon);
     console.log('hello world');
 
+
+    //error message gets removed when valid item is submitted
     if (document.getElementsByClassName('error-message-div')[0]) {
         toDoBoxParent.removeChild(document.getElementsByClassName('error-message-div')[0]);
     }
@@ -69,9 +80,9 @@ deleteIcon.addEventListener('click', deleteTask);
 }
 
 function clearItems() {
-
     const itemsList = document.getElementsByClassName('item-list')[0];
 
+    //checks and deletes element(child) if its existence === true
     while (itemsList.firstChild) {
         itemsList.removeChild(itemsList.firstChild)
     }
@@ -79,18 +90,70 @@ function clearItems() {
 
 
 
-function taskFinished() {
+function taskFinished(taskFinished) {
 
     const checkIcon = document.getElementById('task-finished');
+    const hardcodedCheckIcon = document.getElementById('task-finished-hard')
 
-    this.parentNode.parentNode.style.backgroundColor = 'lime';
+    /* making a difference between the hardcoded initial task
+     and the self entered one since for some reason the dynamically
+     created ones and the hardcoded one work differently */
+
+    if (taskFinished === hardcodedCheckIcon) {
+        taskFinished.parentNode.parentNode.style.backgroundColor = 'lime'
+    }
+    else if (checkIcon) {
+        
+        this.parentNode.parentNode.style.backgroundColor = 'lime';
+    }
     
-    //console.log('task finished');
+   // console.log('task finished');
     
 }
 
-function deleteTask() {
-    const deleteIcon = document.getElementById('delete-task');
+function deleteTask(deleteTask) {
 
-    this.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode);
+    const deleteIcon = document.getElementById('delete-task');
+    const hardcodedDeleteIcon = document.getElementById('delete-task-hard')
+
+/* making a difference between the hardcoded initial task
+     and the self entered one since for some reason the dynamically
+     created ones and the hardcoded one work differently */
+
+    if (deleteTask === hardcodedDeleteIcon){
+        deleteTask.parentNode.parentNode.parentNode.removeChild(deleteTask.parentNode.parentNode)
+    }
+    else if (deleteIcon) {
+        this.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode);
+
+    }
+}
+
+function editItem(editItem) {
+    //const itemName = document.getElementById('item-name');
+    const editIcon = document.getElementById('edit-icon')
+    const hardcodedEditIcon = document.getElementById('task-edit-hard')
+
+    /* making a difference between the hardcoded initial task
+     and the self entered one since for some reason the dynamically
+     created ones and the hardcoded one work differently */
+
+    if (editItem === hardcodedEditIcon) {
+      
+        const respectiveParagraph = editItem.parentNode.parentNode.children[0];
+
+        console.log(respectiveParagraph)
+        respectiveParagraph.focus();
+        //respectiveParagraph.select();
+
+    }
+    else if (editIcon)
+    {
+        //this is getting crazy
+        const respectiveParagraph = this.parentNode.parentNode.firstChild;
+        console.log(respectiveParagraph)
+        respectiveParagraph.focus();
+        //respectiveParagraph.select();
+
+    }
 }
